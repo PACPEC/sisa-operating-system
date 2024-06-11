99c9693db08bf68c24c9db5c60001e348c3a03b1
serikpg <serikpgp@gmail.com>
2024-06-11 19:40:35 +0200

    Inline assembly per a saltar a mode usuari

--- kernel.c
+++ kernel.c
@@ -1,8 +1,18 @@
-__attribute__((noreturn)) void umain();
-
 __attribute__((noreturn)) void kmain() {
-  char * vga = 0xA000;
-  *vga = 'A';
-  umain();
-  for (;;);
+  // User space switch
+  int *userpc = 0x0000;
+  __asm__ (
+    "wrs s0, %0\n\t"
+    "wrs s1, %1\n\t"
+    "wrs s2, %2\n\t"
+    "wrs s3, %3\n\t"
+    "reti"
+    : // no output operands
+    : "r"(0x0002 /*interrupts enable, user mode*/), 
+      "r"(userpc), 
+      "r"(14 /*syscall interrupt code*/), 
+      "r"(0 /*syscall code, 0 by default*/)
+    : "s0", "s1", "s2", "s3" /*acknowledge battered registers to compiler*/);
+
+  for (;;); // infinte loop - Instead of halting
 }
